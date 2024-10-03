942acba5dbfc704ce35470fd8c646e9d7b5f549f
jtreutel <jtreutel@users.noreply.github.com>
2024-10-03 09:47:06 +0900

    docs(services/vars): correct comments

--- terraform/services/variables.tf
+++ terraform/services/variables.tf
@@ -1,17 +1,17 @@
 variable "chart_version_prometheus" {
-  description = "kube-prometheus-stack version. Note that this is different than the Prometheus app version."
+  description = "kube-prometheus-stack chart version. Note that this is different than the Prometheus app version."
   type = string
   default = "63.1.0" #app ver: v0.76.1
 }
 
 variable "chart_version_ingress_nginx" {
-  description = "ingress-nginx version. Note that this is different than the Prometheus app version."
+  description = "ingress-nginx chart version. Note that this is different than the Ingress Nginx app version."
   type = string
   default = "4.11.2" #app ver: 1.11.2
 }
 
 variable "chart_version_argo_cd" {
-  description = "argo-cd version. Note that this is different than the Prometheus app version."
+  description = "argo-cd chart version. Note that this is different than the ArgoCD app version."
   type = string
   default = "7.6.7" #app ver: v2.12.4
 }
